255cde56423cef4e3bbc258357893c033bfdab23
jason0320 <jasonnosaj0320@gmail.com>
2024-12-15 12:17:16 +0800

    update

--- Ench_Gen_Patch.cs
+++ Ench_Gen_Patch.cs
@@ -17,7 +17,6 @@ internal class Ench_Gen_Patch
         List<SourceElement.Row> list = new List<SourceElement.Row>();
         int num = 0;
         int num2 = lv + 5 + EClass.rndSqrt(10);
-        float num3 = (float)(3 + Mathf.Min(lv / 10, 15)) + Mathf.Sqrt((float)lv);
         foreach (SourceElement.Row row in EClass.sources.elements.rows)
         {
             if ((!neg || !row.tag.Contains("flag")) && func(row) && row.LV < num2)
@@ -30,17 +29,18 @@ internal class Ench_Gen_Patch
         {
             return null;
         }
-        int num4 = EClass.rnd(num);
-        int num5 = 0;
-        foreach (SourceElement.Row row2 in list)
+        int num3 = EClass.rnd(num);
+        int num4 = 0;
+        foreach (SourceElement.Row item in list)
         {
-            num5 += row2.chance;
-            if (num4 < num5)
+            num4 += item.chance;
+            if (num3 < num4)
             {
-                string category = EClass.sources.elements.map[row2.id].category;
-                bool flag = category == "skill" || category == "attribute" || category == "resist" || category == "ability";
-                int item = (row2.mtp + EClass.rnd(row2.mtp + (int)num3)) / row2.mtp * ((flag && neg) ? -1 : 1);
-                return new Tuple<SourceElement.Row, int>(row2, item);
+                string text = EClass.sources.elements.map[item.id].category;
+                bool flag = text == "skill" || text == "attribute" || text == "resist" || text == "ability";
+                float num5 = (float)(3 + Mathf.Min(lv / 10, 15)) + Mathf.Sqrt(lv * item.encFactor / 100);
+                int num6 = (item.mtp + EClass.rnd(item.mtp + (int)num5)) / item.mtp * ((!(flag && neg)) ? 1 : (-1));
+                return new Tuple<SourceElement.Row, int>(item, num6);
             }
         }
         return null;
